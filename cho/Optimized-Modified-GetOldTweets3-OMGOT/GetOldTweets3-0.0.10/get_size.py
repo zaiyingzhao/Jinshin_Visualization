8df6748817c1ffeaf88b24c9cd62e3073b621f12
zaiyingzhao <zaiyingzhao@g.ecc.u-tokyo.ac.jp>
2022-11-08 16:24:27 +0900

    各人身事故の関連ツイート検索テンプレート

--- cho/Optimized-Modified-GetOldTweets3-OMGOT/GetOldTweets3-0.0.10/get_size.py
+++ cho/Optimized-Modified-GetOldTweets3-OMGOT/GetOldTweets3-0.0.10/get_size.py
@@ -2,17 +2,37 @@ import subprocess
 from subprocess import PIPE
 import csv
 import pandas as pd
+from datetime import datetime as dt
+import datetime
+import time
 
-# 今後以下の情報をcsvから読み取ってツイート数を取得
-since = "2021-11-01 00:59:00"
-until = "2021-11-01 01:00:00"
-keyword = "京王線"
+start = time.time()
+jinshin = pd.read_csv("jinshin2.csv")
+tweets = []
 
-cp = subprocess.run(["python3","cli.py","-s",keyword,"--since",since,"--until",until],encoding="utf-8", stdout=PIPE)
+for i in range(len(jinshin)):
+    since = jinshin["date"][i] + ":00"
+    since = dt.strptime(since, "%Y/%m/%d %H:%M:%S")
+    keyword = jinshin["place"][i]
+    until = since + datetime.timedelta(hours=2)
+    since = since.strftime("%Y-%m-%d %H:%M:%S")
+    until = until.strftime("%Y-%m-%d %H:%M:%S")
+    cp = subprocess.run(
+        ["python3", "cli.py", "-s", keyword, "--since", since, "--until", until],
+        encoding="utf-8",
+        stdout=PIPE,
+    )
 
-with open("user.csv", "wt") as f:
-    writer = csv.writer(f, lineterminator="\n")
-    writer.writerow([cp.stdout])
+    with open("user.csv", "wt") as f:
+        writer = csv.writer(f, lineterminator="\n")
+        writer.writerow([cp.stdout])
 
-# csvの行数確認
-print(sum(1 for line in open("user.csv", "rb")))
+    # csvの行数確認
+    size = sum(1 for line in open("user.csv", "rb"))
+    print(i, size)
+    tweets.append(size)
+
+jinshin["tweets"] = tweets
+jinshin.to_csv("tweets.csv")
+
+print(time.time() - start)
